5640647b59bf1e60f7a2d4c7cee0895ddb29730b
libaoan <287639598@qq.com>
2015-09-22 23:58:03 +0800

    1、商品详细页增加图片左右滚动 2、图册预览增加延迟加载

--- Public/skin/bagsloves/js/goods.js
+++ Public/skin/bagsloves/js/goods.js
@@ -1,5 +1,41 @@
 $(function(){
 	
+	/** 图片左右滚动 **/
+	var oPic = $('#slider_pic').find('ul');
+	var oImg = oPic.find('li');
+	var oLen = oImg.length;
+	var oLi = oImg.width();
+	var prev = $("#prev");
+	var next = $("#next");
+	var step = 94;
+	
+	oPic.width(oLen*step); //计算总长度
+	var iNow = 0;
+	var iTimer = null;
+	
+	prev.click(function(){
+		if(iNow>0){ 
+			iNow--;
+		}
+		ClickScroll();
+	});
+	next.click(function(){
+		if(iNow<oLen-3){
+			iNow++
+		}
+		ClickScroll();
+	});
+	
+	function ClickScroll()
+	{
+		iNow==0? prev.addClass('no_click'): prev.removeClass('no_click');
+		iNow==oLen-3?next.addClass("no_click"):next.removeClass("no_click");
+		oPic.animate({left:-iNow*step});
+	}
+	/** 图片左右滚动 end **/
+
+	
+	
 	$("#continue_shopping").click(function(){
 		$("#cart_ok").hide();
 	});
