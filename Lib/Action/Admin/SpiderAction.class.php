f09aeb1b047630aeb9efec6542a1f750684c7222
libaoan <287639598@qq.com>
2015-10-13 22:56:08 +0800

    1、调试采集入库接口 2、调整列表页和搜索页模板显示

--- Lib/Action/Admin/SpiderAction.class.php
+++ Lib/Action/Admin/SpiderAction.class.php
@@ -123,6 +123,119 @@ class SpiderAction extends Action {
 		$width = GetValue('ImgThumbW');
 		$height = GetValue('ImgThumbH');
 		
+		$max_id = M('CjDetail')->max('id');
+		for($i=1; $i<=$max_id; $i++) {
+			$info = M('CjDetail')->where("id={$i} AND status=2 and site_name='2015destinationbags'")->find();
+			if(empty($info) || $info['is_push'] == '1') continue;
+						
+			$related_ids = $info['related_id']? $info['related_id']: $info['goods_id'];
+			$relate_goods = M('CjDetail')->where("goods_id in($related_ids) AND status=2 and site_name='2015destinationbags' AND is_push=0")->order('id')->select();
+			if($relate_goods) {
+				$insert_id = array();
+				foreach($relate_goods as $k=>$val) {
+					$data = array();
+					$data['name'] = $val['goods_name'];
+					$data['price'] = $val['market_price'];
+					$data['pricespe'] = $val['shop_price'];
+					$data['cateid'] = $this->cate[$val['cat_id']];
+					$data['remark'] = $val['desc'];
+					$data['dateline'] = time();
+					$data['cj_url'] = $val['url'];
+					$data['brandid'] = 0;
+					$data['costprice'] = 0;
+					$data['provider'] = 0;
+					$insert_id[] = $this->_add($val['id'], $val['attr'], $val['select_color'], $val['attachment'], $data);
+				}
+				//插入商品关联关系
+				$this->add_related($insert_id);
+			}
+		}
+
+	}
+	
+	public function add_related($goods_ids)
+	{
+		if(empty($goods_ids) || count($goods_ids) == 1) return false;
+		
+		for($i=0; $i<count($goods_ids); $i++) {
+			$curr_id = $goods_ids[$i];
+			$tmp_goods_ids = $goods_ids;
+			$key = array_search($curr_id, $tmp_goods_ids);
+			
+			if($key !== false) {
+				array_splice($tmp_goods_ids, $key, 1);
+			}
+			
+			for($j=0; $j<count($tmp_goods_ids); $j++) {
+				M('ProductsRelated')->add(array('products_id'=>$curr_id, 'related_products_id'=>$tmp_goods_ids[$j]));
+			}
+		}
+	}
+	
+	
+	public function _add($id, $attr, $curr_color, $attachment, $data)
+	{
+		if(empty($id) || empty($data) || !is_array($data)) return false;
+		
+		$width = GetValue('ImgThumbW');
+		$height = GetValue('ImgThumbH');
+
+		//添加商品图册
+		if($goods_id = M('Products')->add($data)) {
+			$attachment = explode('||', $attachment);
+			for($i=0; $i<count($attachment); $i++) {
+				$img[$i]['img_url'] = $this->http_down_img($attachment[$i]);
+				$img[$i]['thumb_url'] = $this->ceate_thumb($img[$i]['img_url'], $width, $height);
+				$img[$i]['img_original'] = $img[$i]['thumb_url'];
+				$img[$i]['img_desc'] = end(pathinfo($img[$i]['img_url']));
+			}
+			foreach($img as $thumb) {
+				M('ProductsGallery')->add(array('pid'=>$goods_id, 'img_url'=>$thumb['img_url'], 'thumb_url'=>$thumb['thumb_url'], 'img_desc'=>$thumb['img_desc'], 'img_original'=>$thumb['img_original']));
+			}
+			$serial = 'B0000'.$goods_id;
+			M('Products')->where("id='{$goods_id}'")->save(array('serial'=>$serial, 'bigimage'=>$img[0]['img_url'], 'smallimage'=>$img[0]['thumb_url']));
+			
+			$attr_list = unserialize($attr);
+			if(!empty($attr_list) && is_array($attr_list)) {
+				//添加属性-尺寸
+				if($attr_list['size']) {
+					$size_data['products_id'] = $goods_id;
+					$size_data['attr_id'] = 2;
+					$size_data['attr_value'] = $attr_list['size'];
+					$size_data['dateline'] = time();
+					$size_data['sort'] = 0;
+					M('ProductsAttr')->add($size_data);
+				}
+			}
+			
+			//添加属性-颜色
+			if($curr_color) {
+				$color = unserialize($curr_color);
+				$color['img'] = str_replace('_0.', '.', $color['img']);
+				$color_data['img_url'] = $this->http_down_img($color['img']);
+				$color_data['thumb_url'] = $this->ceate_thumb($color_data['img_url'], $width, $height);
+				$color_data['products_id'] = $goods_id;
+				$color_data['attr_id'] = 3;
+				$color_data['attr_value'] = $color['color'];
+				$color_data['dateline'] = time();
+				$color_data['sort'] = 0;
+				M('ProductsAttr')->add($color_data);
+			}
+			
+			M('CjDetail')->where("id='{$id}'")->save(array('is_push'=>1));
+			echo "compelte {$id}\n<br>";
+			unset($data,$attr_list,$size_data,$color_data,$img);
+			usleep(500000);
+			return $goods_id;
+		}
+		return false;
+	}
+	
+	public function init_bak()
+	{
+		$width = GetValue('ImgThumbW');
+		$height = GetValue('ImgThumbH');
+		
         $list = M('CjDetail')->where("status=1 and site_name='2015destinationbags' AND is_use=2 AND is_push=0")->order('id')->select();
 		//$list = M('CjDetail')->where("goods_id in(6340,6341) AND is_use=2 AND is_push=0")->order('id')->limit(30)->select();
 		foreach($list as $k=>$val) {
